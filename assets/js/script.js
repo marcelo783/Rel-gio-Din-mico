f304346f8c1baea68d4f024afa628128ef31e9b2
marcelo783 <marcelo.oliveira7931@gmail.com>
2023-11-21 11:04:24 -0300

    Corrigir o código JavaScript para contar corretamente as horas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
-const horas = document.getElementById('horas');
-const minutos = document.getElementById('minutos');
-const segundos = document.getElementById('segundos');
+
+const horasElement = document.getElementById('horas');
+const minutosElement = document.getElementById('minutos');
+const segundosElement = document.getElementById('segundos');
 
 const relogio = setInterval(function time(){
     let dateToday = new  Date();
-    let hr= dateToday.getHours();
-    let min= dateToday.getMinutes();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
 
-    if(hr < 10) horas = '0' + hr;
+    if(hr < 10) hr = '0' + hr;
+    if(min < 10) min = '0' + min;
+    if(seg < 10) seg = '0' + seg;
 
-    if(min < 10) horas = '0' + min;
+    horasElement.textContent = hr;
+    minutosElement.textContent = min;
+    segundosElement.textContent = seg;
 
-    if(seg < 10) horas = '0' + seg;
-
-    horas.textContent = hr;
-    minutos.textContent = min;
-    segundos.textContent = seg;
-
-})
+}, 1000);
